9766def5356a1a24d68b88e0a9e81c5e5cb50660
kshamata7 <139546066+kshamata7@users.noreply.github.com>
2024-10-05 21:52:19 +0530

    Update main.tf

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -119,12 +119,7 @@ resource "aws_security_group" "private_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
   egress {
     from_port   = 0
     to_port     = 0
